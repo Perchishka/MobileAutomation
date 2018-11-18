ab7d21e1d92bd8bb577ac6f4ee29c7500cd11851
perceva <ekaterina_perets@mail.ru>
2018-11-18 23:23:58 +0300

    1st_task_2nd_Module

--- src/test/java/FistTest.java
+++ src/test/java/FistTest.java
@@ -1,9 +1,14 @@
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
 
@@ -12,27 +17,97 @@ public class FistTest {
 
     //лучше не менять, стандартное название метода аппиума. именно так он понимает откуда брать параметры
     @Before
-    public void setUp()throws Exception{
+    public void setUp() throws Exception {
         DesiredCapabilities capabilities = new DesiredCapabilities();
 
-        capabilities.setCapability("platformName","Android");
+        capabilities.setCapability("platformName", "Android");
         // для андройда валью может быть любым а для айоса должно четко соответствовать названию девайса
-        capabilities.setCapability("deviceName","AndroidTestDevice");
-        capabilities.setCapability("platformVersion","6.0");
-        capabilities.setCapability("automationName","Appium");
-        capabilities.setCapability("appPackage","org.wikipedia");
-        capabilities.setCapability("appActivity",".main.MainActivity");
-        capabilities.setCapability("app","C:\\Users\\Dell\\Desktop\\appium\\apks\\org.wikipedia.apk");
+        capabilities.setCapability("deviceName", "AndroidTestDevice");
+        capabilities.setCapability("platformVersion", "6.0");
+        capabilities.setCapability("automationName", "Appium");
+        capabilities.setCapability("appPackage", "org.wikipedia");
+        capabilities.setCapability("appActivity", ".main.MainActivity");
+        capabilities.setCapability("app", "C:\\Users\\Dell\\Desktop\\appium\\apks\\org.wikipedia.apk");
 
         driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
 
     }
+
     @After
-    public void tearDown(){
+    public void tearDown() {
         driver.quit();
     }
     @Test
-    public void firstTest(){
-        System.out.println("First test run");
+    public void textBeforeSearch(){
+       String searchResult = waitForElementPrsenetBy(
+                By.xpath("//*[contains(@text, 'Search Wikipedia')]"),
+               "Search field was not foud")
+                .getAttribute("text");
+        System.out.println(searchResult);
+        Assert.assertTrue(searchResult.contains("Search"));
     }
+   /* @Test
+    public void testCancelSearch() {
+        waitForElementAndClick(By.id("org.wikipedia:id/search_container"),
+                "Cannot find SearchInput",
+                15);
+
+        waitForElementAndClick(
+                By.id("org.wikipedia:id/search_close_btn"),
+                "Cannot find SearchInput",
+                15);
+
+        waitForElementNotPresented(By.id("org.wikipedia:id/search_close_btn"),
+                "Element is still presented on the page",
+                15);
+    }*/
+   /* @Test
+    public void firstTest() {
+
+        waitForElementAndClick(By.xpath("//*[contains(@text, 'Search Wikipedia')]"),
+                "Cannot find SearchInput", 5);
+
+        waitForElementAndSendkeys(By.xpath("//*[contains(@text, 'Search…')]"),
+                "Cannot find search input", "Java", 5);
+
+
+        waitForElementPrsenetBy(
+                By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_container']" +
+                        "//*[@text='Object-oriented programming language']"),
+                "Java element was not found", 15);
+        //System.out.println("First test run");
+    }*/
+
+    private WebElement waitForElementPrsenetBy(By by, String erroeMessage, long timeoutInSeconds) {
+        WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+        wait.withMessage(erroeMessage + "\n");
+        return wait.until(ExpectedConditions.presenceOfElementLocated(by));
+    }
+
+
+    private WebElement waitForElementPrsenetBy(By by, String errorMessage) {
+
+        return waitForElementPrsenetBy(by, errorMessage, 10);
+    }
+
+    private WebElement waitForElementAndClick(By by, String errorMessage, long timeoutInSeconds) {
+        WebElement element = waitForElementPrsenetBy(by, errorMessage, timeoutInSeconds);
+        element.click();
+        return element;
+    }
+
+    private WebElement waitForElementAndSendkeys(By by, String errorMessage, String value, long timeoutInSeconds) {
+        WebElement element = waitForElementPrsenetBy(by, errorMessage, timeoutInSeconds);
+        element.sendKeys(value);
+        return element;
+    }
+
+    private boolean waitForElementNotPresented(By by, String errorMessage, long timeoutInSeconds) {
+        WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+        wait.withMessage(errorMessage + "\n");
+        return wait.until(ExpectedConditions.invisibilityOfElementLocated(by));
+    }
+
+
+
 }
