d903faf61c2e3722e9bc7b5dd53e1a9054a17066
perceva <ekaterina_perets@mail.ru>
2018-11-30 00:08:49 +0300

    swipe test

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,8 +1,10 @@
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -11,12 +13,16 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.net.URL;
 import java.util.List;
 
+import static io.appium.java_client.touch.WaitOptions.waitOptions;
+import static io.appium.java_client.touch.offset.PointOption.point;
+import static java.time.Duration.ofSeconds;
+
 public class BaseTest {
     private AppiumDriver driver;
 
-     public AppiumDriver  getDriver(){
-         return driver;
-     }
+    public AppiumDriver getDriver() {
+        return driver;
+    }
 
     //лучше не менять, стандартное название метода аппиума. именно так он понимает откуда брать параметры
     @Before
@@ -26,7 +32,7 @@ public class BaseTest {
         capabilities.setCapability("platformName", "Android");
         // для андройда валью может быть любым а для айоса должно четко соответствовать названию девайса
         capabilities.setCapability("deviceName", "AndroidTestDevice");
-        capabilities.setCapability("platformVersion", "6.0");
+        capabilities.setCapability("platformVersion", "8.0");
         capabilities.setCapability("automationName", "Appium");
         capabilities.setCapability("appPackage", "org.wikipedia");
         capabilities.setCapability("appActivity", ".main.MainActivity");
@@ -70,6 +76,23 @@ public class BaseTest {
     private boolean waitForListOfWebElementsNotPresented(List<WebElement> elements) {
         WebDriverWait wait = new WebDriverWait(driver, 10);
         return wait.until(ExpectedConditions.invisibilityOfAllElements(elements));
+    }
+
+    protected void swipeUp(int timeOfSwipe) {
+        TouchAction action = new TouchAction(driver);
+        Dimension size = driver.manage().window().getSize();
+
+        int x = size.width/2;
+        int start_y = (int) (size.height*0.8);
+        int end_y = (int) ( size.height * 0.2);
+
+        //perform  отправляет всю последовательность действий на выполнение
+        action
+                .press(point(x, start_y))
+                .waitAction(waitOptions(ofSeconds(timeOfSwipe)))
+                .moveTo(point(x, end_y))
+                .release()
+                .perform();
     }
 
     @After
---
+++ src/test/java/SwipeTest.java
@@ -0,0 +1,32 @@
+import io.appium.java_client.AppiumDriver;
+import org.junit.Test;
+import org.openqa.selenium.By;
+
+public class SwipeTest extends BaseTest {
+    BaseTest test = new BaseTest();
+    AppiumDriver driver = test.getDriver();
+
+    @Test
+    public void swipeTest() {
+        waitForElementAndClick(By.xpath("//*[contains(@text, 'Search Wikipedia')]"),
+                "Cannot find Search input", 5);
+
+        waitForElementAndSendkeys(By.xpath("//*[contains(@text, 'Search…')]"),
+                "Cannot find search input", "Java", 5);
+
+        waitForElementAndClick(
+                By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_container']" +
+                        "//*[@text='Object-oriented programming language']"),
+                "Java element was not found", 15);
+
+        waitForElementPrsenetBy(
+                By.id("org.wikipedia:id/view_page_title_text"),
+                "Page Title was not found", 15);
+
+        swipeUp(2000);
+
+
+
+    }
+
+}
